2e87e0264aaca930fa49ddaf378f42a31afda1e7
rajip <rajipmhz450@gmail.com>
2026-01-29 16:35:49 +0545

    change server.js

--- src/server.js
+++ src/server.js
@@ -14,12 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello world");
